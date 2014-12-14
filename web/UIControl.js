1ceb6f32f0b769dd918700cdb2886d9fb24b1b79
tongium <mr.gnot@gmail.com>
2014-12-14 15:45:01 +0700

    remove uiControl and add code to index

--- web/UIControl.js
+++
@@ -1,9 +0,0 @@
-function overlay(n) {
-    el = document.getElementById("overlay" + n);
-    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-}
-
-
-function scrollWin() {
-    window.scrollTo(0, 0);
-}
